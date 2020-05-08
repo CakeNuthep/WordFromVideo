9b79908e745a9cba200a94f48fbe22b06641196b
CakeNuthep <49687834+CakeNuthep@users.noreply.github.com>
2020-05-09 06:46:34 +0700

    first commit

---
+++ application/WordFromPicture/WordFromPicture/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace WordFromPicture
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox_input = new System.Windows.Forms.PictureBox();
+            this.textBox_pathFile = new System.Windows.Forms.TextBox();
+            this.button_browse = new System.Windows.Forms.Button();
+            this.button_run = new System.Windows.Forms.Button();
+            this.button_close = new System.Windows.Forms.Button();
+            this.textBox_word = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_input)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(709, 129);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.SkyBlue;
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(0, 119);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(709, 10);
+            this.panel3.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label1.Location = new System.Drawing.Point(177, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(357, 44);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Word From Picture";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.AliceBlue;
+            this.panel2.Controls.Add(this.textBox_word);
+            this.panel2.Controls.Add(this.pictureBox_input);
+            this.panel2.Controls.Add(this.textBox_pathFile);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 129);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(546, 475);
+            this.panel2.TabIndex = 1;
+            // 
+            // pictureBox_input
+            // 
+            this.pictureBox_input.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox_input.Image = global::WordFromPicture.Properties.Resources._default;
+            this.pictureBox_input.Location = new System.Drawing.Point(60, 144);
+            this.pictureBox_input.Name = "pictureBox_input";
+            this.pictureBox_input.Size = new System.Drawing.Size(430, 319);
+            this.pictureBox_input.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox_input.TabIndex = 1;
+            this.pictureBox_input.TabStop = false;
+            // 
+            // textBox_pathFile
+            // 
+            this.textBox_pathFile.Location = new System.Drawing.Point(60, 40);
+            this.textBox_pathFile.Name = "textBox_pathFile";
+            this.textBox_pathFile.Size = new System.Drawing.Size(430, 22);
+            this.textBox_pathFile.TabIndex = 0;
+            // 
+            // button_browse
+            // 
+            this.button_browse.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_browse.Location = new System.Drawing.Point(580, 160);
+            this.button_browse.Name = "button_browse";
+            this.button_browse.Size = new System.Drawing.Size(99, 41);
+            this.button_browse.TabIndex = 2;
+            this.button_browse.Text = "Browse";
+            this.button_browse.UseVisualStyleBackColor = true;
+            this.button_browse.Click += new System.EventHandler(this.button_browse_Click);
+            // 
+            // button_run
+            // 
+            this.button_run.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_run.Location = new System.Drawing.Point(580, 221);
+            this.button_run.Name = "button_run";
+            this.button_run.Size = new System.Drawing.Size(99, 41);
+            this.button_run.TabIndex = 3;
+            this.button_run.Text = "Run";
+            this.button_run.UseVisualStyleBackColor = true;
+            this.button_run.Click += new System.EventHandler(this.button_run_Click);
+            // 
+            // button_close
+            // 
+            this.button_close.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_close.Location = new System.Drawing.Point(580, 551);
+            this.button_close.Name = "button_close";
+            this.button_close.Size = new System.Drawing.Size(99, 41);
+            this.button_close.TabIndex = 4;
+            this.button_close.Text = "Close";
+            this.button_close.UseVisualStyleBackColor = true;
+            this.button_close.Click += new System.EventHandler(this.button_close_Click);
+            // 
+            // textBox_word
+            // 
+            this.textBox_word.Location = new System.Drawing.Point(60, 101);
+            this.textBox_word.Name = "textBox_word";
+            this.textBox_word.Size = new System.Drawing.Size(438, 22);
+            this.textBox_word.TabIndex = 2;
+            this.textBox_word.Text = "X";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(709, 604);
+            this.Controls.Add(this.button_close);
+            this.Controls.Add(this.button_run);
+            this.Controls.Add(this.button_browse);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_input)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox_input;
+        private System.Windows.Forms.TextBox textBox_pathFile;
+        private System.Windows.Forms.Button button_browse;
+        private System.Windows.Forms.Button button_run;
+        private System.Windows.Forms.Button button_close;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox textBox_word;
+    }
+}
+
---
+++ application/WordFromPicture/WordFromPicture/Form1.cs
@@ -0,0 +1,113 @@
+﻿using Accord.Video.FFMPEG;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WordFromPicture
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button_browse_Click(object sender, EventArgs e)
+        {
+            using(OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.RestoreDirectory = true;
+                if(openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    textBox_pathFile.Text = openFileDialog.FileName;
+                    VideoFileReader reader = new VideoFileReader();
+                    reader.Open(textBox_pathFile.Text);
+                    pictureBox_input.Image = resizeImage(reader.ReadVideoFrame(),new Size(380,100));
+                    reader.Close();
+                }
+            }
+        }
+
+        public static Image resizeImage(Image imgToResize, Size size)
+        {
+            return (Image)(new Bitmap(imgToResize, size));
+        }
+
+        private void button_run_Click(object sender, EventArgs e)
+        {
+            VideoFileReader reader = new VideoFileReader();
+            reader.Open(textBox_pathFile.Text);
+            using (WordprocessingDocument wordDocument = WordprocessingDocument.Create(@"C:\Users\Cake\Desktop\test.docx", DocumentFormat.OpenXml.WordprocessingDocumentType.Document))
+            {
+                MainDocumentPart mainPart = wordDocument.AddMainDocumentPart();
+
+                mainPart.Document = new Document();
+                Body body = mainPart.Document.AppendChild(new Body());
+                ParagraphProperties bodyProperty = new ParagraphProperties();
+                Justification justification1 = new Justification() { Val = JustificationValues.Center };
+                bodyProperty.Append(justification1);
+                Paragraph para = body.AppendChild(new Paragraph(bodyProperty));
+
+                int iterator = (int)(reader.FrameRate / 3);
+
+                for (int i = 0; i < 2000; i += iterator)
+                {
+                    Bitmap videoFrame = reader.ReadVideoFrame(i);
+                    Bitmap image = (Bitmap)resizeImage(videoFrame, new Size(380, 100));
+                    string str = textBox_word.Text;
+                    int indexLetter = 0;
+                    for (int y = 0; y < image.Height; y++)
+                    {
+                        for (int x = 0; x < image.Width; x++)
+                        {
+
+                            System.Drawing.Color color = image.GetPixel(x, y);
+                            DocumentFormat.OpenXml.Wordprocessing.Color wordColor = new DocumentFormat.OpenXml.Wordprocessing.Color() { Val = color.R.ToString("X2") + color.G.ToString("X2") + color.B.ToString("X2") };
+                            FontSize fontSize = new FontSize() { Val = "12" };
+
+                            RunProperties runPro = new RunProperties();
+                            runPro.Append(wordColor);
+                            runPro.Append(fontSize);
+                            
+                            Run run = para.AppendChild(new Run());
+                            run.AppendChild(runPro);
+                            char letter = str[indexLetter];
+
+                            run.AppendChild(new Text(letter.ToString()));
+
+                            if (str.Length > 0)
+                            {
+                                indexLetter = (indexLetter + 1) % str.Length;
+                            }
+                        }
+                        Run runNewLine = para.AppendChild(new Run());
+                        runNewLine.AppendChild(new Break());
+                    }
+
+                    Run runBreakPage = para.AppendChild(new Run());
+                    runBreakPage.AppendChild(new Break() { Type=BreakValues.Page});
+
+                    videoFrame.Dispose();
+                    image.Dispose();
+                }
+            }
+            
+            reader.Close();
+            MessageBox.Show("finish");
+
+        }
+    }
+}
